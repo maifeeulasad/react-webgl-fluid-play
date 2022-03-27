f8874c55af39d2cea5cbaf5b332af67436790543
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-03-27 16:15:32 +0600

    //wip

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Canvas from './Canvas';
+// import Canvas from './Canvas';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Canvas height={window.innerHeight} width={window.innerWidth} />
+      {/*
+      <Canvas height={window.innerHeight} width={window.innerWidth} />
+      */}
   </React.StrictMode>,
   document.getElementById('root')
 );
